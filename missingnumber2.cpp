e277e7f26e618be66465b85d03f399a22629da80
Nidhi Sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2024-06-13 15:30:44 +0530

    Add files via upload

---
+++ missingnumber2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+vector<int>count(N,0)
+for(i=0; i<N; i++)
+count[arr[i]-1]
+//using//
+for(int i=0; i<N; i++)
+{
+	if(count[i]==0)
+	{
+		cout<<i++;
+		break;
+	}
+}
+
