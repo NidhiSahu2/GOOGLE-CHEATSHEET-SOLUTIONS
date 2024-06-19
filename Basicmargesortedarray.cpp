2f8247e23c90f6ce78ad14341f8fe6656506ece3
Nidhi Sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2024-06-19 23:04:14 +0530

    Add files via upload

---
+++ Basicmargesortedarray.cpp
@@ -0,0 +1,16 @@
+class solutions
+{
+	pubilc:
+void marge(vector<int>&num1,int m,int n vector<int>num2)
+int i=m-1;
+int j=n-1;
+int k = m+n-1;
+while(j>=0)
+{
+	if(i>=0&&num[i]>num[2])
+	nums1[k--]=num[i--]
+	else 
+	if(i<=0&&num[i]<num[2])
+	nums1[k--]=num[j--]
+}
+}
